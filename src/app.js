cfe8c52487ffa32bd6dea63582e63b85b5b8822e
GustavoBores <binarymasterjobs@gmail.com>
2023-08-21 13:02:06 -0300

    messages corrigido retorno de status

--- src/app.js
+++ src/app.js
@@ -86,24 +86,30 @@ app.get("/participants", async (req, res) => {
 
 app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
-    const { name } = req.headers;
-
-    const typeAccepted = ["message", "private_message"];
+    const { user } = req.headers;
 
     const schema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().required().valid(...typeAccepted)
+        type: joi.string().required()
     });
 
     const validationErrors = validateSchema(schema, req.body);
 
+    if ( type !== "message" || type !== "private_message" ) {
+        return res.status(422)
+    }
+
+    if ( !user ) {
+        return res.status(422)
+    }
+
     if (validationErrors) {
-        return res.status(422).send(validationErrors);
+        return res.status(422)
     }
 
     try {
-        const sender = await db.collection("participants").findOne({ name });
+        const sender = await db.collection("participants").findOne({ user });
 
         if (!sender) {
             return res.sendStatus(422);
